3989f05c048eb1d1dc9d2cd41257bf046338f79d
Ivana Lemma <ivalemma@gmail.com>
2022-10-18 19:50:19 -0300

    completamos pagina de articulo tintes naturales

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let edadMinima = 18;
-let edadUsuario = prompt("Ingrese su edad");
+//let edadMinima = 18;
+/* let edadUsuario = prompt("Ingrese su edad");
 
 if(edadUsuario >= edadMinima) {
     alert("Podes ingresar a donar");
 } else {
     alert("No podes donar")
-}
+} */
 
 function validacion(){
     alert("Tus mensaje se envio correctamente")
