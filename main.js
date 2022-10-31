1a6caac97d89f17304bbd2755f1b1228b737f98d
Ivana Lemma <ivalemma@gmail.com>
2022-10-31 20:46:39 -0300

    carrusel funcionando con sj

--- main.js
+++ main.js
@@ -11,4 +11,28 @@ function validacion(){
     alert("Tus mensaje se envio correctamente")
 }
 
+/* carrusel funcionamiento si dios quiere */
+
+const carrusel = document.querySelector(".carrusel-items");
+
+let intervalo = null;
+let velocidad = 1;
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+
+const start = () => {
+    intervalo = setInterval(function () {
+        carrusel.scrollLeft = carrusel.scrollLeft + velocidad;
+        if (carrusel.scrollLeft === maxScrollLeft) {
+            velocidad = velocidad * -1;
+        } else if (carrusel.scrollLeft === 0) {
+            velocidad = velocidad * -1;
+        }
+    }, 10 )
+};
+
+const stop = () => {
+
+};
+
+start();
 
