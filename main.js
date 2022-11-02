440a80214ec5358a53e450c8cb8ebc6297e3cf28
Ivana Lemma <ivalemma@gmail.com>
2022-11-02 10:11:11 -0300

    carrusel con fondo y hoverstate listos

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ const carrusel = document.querySelector(".carrusel-items");
 
 let intervalo = null;
 let velocidad = 1;
+//como sacar el scroll maximo para hacer despues que el scroll vuelva
 let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
-
+//scroll width 
 const start = () => {
     intervalo = setInterval(function () {
         carrusel.scrollLeft = carrusel.scrollLeft + velocidad;
@@ -31,8 +32,19 @@ const start = () => {
 };
 
 const stop = () => {
-
+    clearInterval(intervalo);
 };
+
+//hacer un evento que cuando paso el mouse se agrande la imagen, se usa mouseover y mouseout
+carrusel.addEventListener("mouseover", () => {
+    stop();
+})
+carrusel.addEventListener("mouseout", () => {
+    start();
+})
+
+
+
 
 start();
 
